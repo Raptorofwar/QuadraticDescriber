7a7fe19a884e17606aed641f0cda2c67a06aedf4
Raptorofwar <raptorofwar@gmail.com>
2018-10-05 09:51:38 -0700

    Temporary code.

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,12 @@
 
 public class Quadratic {
+	public static String quadrDescriber(double a, double b, double c) {
+		boolean opensUp;
+		if (a>0) {
+			opensUp=true;
+		}else {
+			opensUp=false;
+		}
+	}
 
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -3,7 +3,15 @@ import java.util.*;
 public class QuadraticClient {
 
 	public static void main(String[] args) {
-		
+		Scanner input=new Scanner(System.in);
+		System.out.print("a: ");
+		double a=input.nextDouble();
+		System.out.print("b: ");
+		double b=input.nextDouble();
+		System.out.print("c: ");
+		double c=input.nextDouble();
+		System.out.println("Description of the graph of: ");
+		System.out.println("y = "+a+"x^2 + "+b+"x + "+c);
 	}
 
 }
