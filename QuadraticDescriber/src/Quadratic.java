f41fc6aab984bdae78e09eff0e15d0f88f91b577
Jeffrey <Jeffrey@LAPTOP-JU64KIIJ>
2018-10-05 20:36:45 -0700

    Adding required methods for quadrDescriber.

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -7,6 +7,103 @@ public class Quadratic {
 		}else {
 			opensUp=false;
 		}
+		
+	}
+	
+	public static String quadform(int a, int b, int c) {
+		
+		//put in 3 ints a, b, and c, in accordance with quadratic formula
+		//returns a string listing roots (x for y=0)
+		
+		double square=discriminant(a,b,c);
+		double squareRoot;
+		if(square<0) {
+			return "no real roots";
+		}
+		squareRoot=sqrt(square);
+		double plusRoot=((-b)+squareRoot)/(2*a);
+		double minusRoot=((-b)-squareRoot)/(2*a);
+		if(plusRoot!=minusRoot) {
+			double maxRoot=round2(max(plusRoot, minusRoot));
+			double minRoot=round2(min(plusRoot, minusRoot));
+			String roots=minRoot+" and "+maxRoot;
+			return roots;
+		}else {
+			return plusRoot+"";
+		}
+	}
+	
+	public static double discriminant (double a, double b, double c) {
+		
+		//takes standard form abc, returns that little bit inside the square root
+		//in quadratic formula
+		
+		return square(b)-4*a*c;
+	}
+	
+	public static double square(double squareInput) {
+		
+		//squares
+		
+		return squareInput*squareInput;
+	}
+	
+	public static double min(double x, double y) {
+		
+		//finds smallest
+		
+		if(x<y) {
+			return x;
+		}else {
+			return y;
+		}
+	}
+	
+	public static double round2(double round) {
+		
+		//rounds input to hundredths place
+		
+		double rounded=round*100;
+		boolean isNegative=false;
+		if (round<0) {
+			rounded*=-1;
+			isNegative=true;
+		}
+		if (rounded%1>=0.5) {
+			rounded+=(1-rounded%1);
+		}else {
+			rounded-=(rounded%1);
+		}
+		if (!isNegative) {
+			return rounded/100;
+		}else {
+			return rounded/=-100;
+		}
+	}
+	
+	public static double max(double x, double y) {
+		
+		//finds largest of two inputs
+		
+		if (x>y) {
+			return x;
+		}else {
+			return y;
+		}
+	}
+	
+	public static double sqrt(double x) {
+		
+		//input double, return sqrt of said double
+		
+		if(x<0) {
+			throw new IllegalArgumentException("Negative square root: "+x);
+		}
+		double guess=x/20;
+		while(absValue(x-square(guess)) >= 0.001){
+			guess-=((square(guess)-x)/(2*guess));
+		}
+		return round2(guess);
 	}
 
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class QuadraticClient {
 
 	public static void main(String[] args) {
+		System.out.println("Provide values for coefficients a, b, and c");
 		Scanner input=new Scanner(System.in);
 		System.out.print("a: ");
 		double a=input.nextDouble();
@@ -12,6 +13,7 @@ public class QuadraticClient {
 		double c=input.nextDouble();
 		System.out.println("Description of the graph of: ");
 		System.out.println("y = "+a+"x^2 + "+b+"x + "+c);
+		System.out.println("Axis of symmetry: ");
 	}
 
 }
